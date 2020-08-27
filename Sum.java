a3db84a85e12660433f9668e26637109aa1d1eae
samhi003 <69116881+samhi003@users.noreply.github.com>
2020-08-27 19:15:33 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,12 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Acess obj = new Acess();
+		System.out.println(obj.num);
+		System.out.println(obj.add(5));
+
+	}
+
+}
+// cannot be compiled as it is a private class
